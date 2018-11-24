648386f256a7c9ab553b41f7f3f2d342c2ade651
Jhon Arias <jhonarmont@hotmail.com>
2018-11-24 14:21:58 -0500

    created a template that will be filled with data from a petition made using axios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,68 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+    <div class="list-container">
+      <h1>Compras del Black Friday</h1>
+      <p v-for="item in shoppings" :key="item.id">
+        {{ item.title }}
+      </p>
+    </div>
   </div>
+
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
-  }
+  },
+  data() {
+    return {
+    shoppings: []
+    }
+  },
+  beforeCreate() {
+    
+  },
+  async created() {
+    console.log('beforeCreate', 1);
+    const url = 'http://jsonplaceholder.typicode.com/posts';
+    const { data } = await axios.get(url)
+      // .catch(() => ({ status: false }))
+    this.shoppings = data
+
+  console.log('beforeCreate', 2, this.shoppings);
+  console.log('beforeCreate', 3); 
+  },
+  beforeMount() {
+    console.log('beforeCreate', this.msg);
+  },
+  mounted() {
+
+  },
+  beforeUpdate() {
+
+  },
+  updated() {
+
+  },
+  beforeDestroy() {
+
+  },
+  destroyed() {
+
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.list-container {
+  background-color: black;
+  color: white;
 }
 </style>
